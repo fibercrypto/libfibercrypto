a616de75b5e54d9ba446f3e64eb0b2ad4a706eb6
Maykel Arias Torres <dev0006@simelo.tech>
2019-11-05 22:36:10 -0500

    [cgo] refs #1 Finalized functions `FC_util_AltcoinCaption`

--- lib/cgo/main.go
+++ lib/cgo/main.go
@@ -4,14 +4,12 @@ libskycoin shim
 package main
 
 import (
-	sky "github.com/fibercrypto/FiberCryptoWallet/src/coin/skycoin/models"
-
-	util "github.com/fibercrypto/FiberCryptoWallet/src/util"
+	
 )
 
 // #cgo CFLAGS: -I../../include
 import "C"
 
 func main() {
-	util.RegisterAltcoin(sky.NewSkyFiberPlugin(sky.SkycoinMainNetParams))
+	
 }
--- lib/cgo/util.pluginutil.go
+++ lib/cgo/util.pluginutil.go
@@ -15,6 +15,7 @@ import "C"
 
 //export FC_util_AltcoinCaption
 func FC_util_AltcoinCaption(_arg0 string, _arg1 *C.GoString_) (____error_code uint32) {
+	callNode()
 	arg0 := string(_arg0)
 	arg1 := util.AltcoinCaption(arg0)
 	copyString(arg1, _arg1)
