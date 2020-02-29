8b2b67d6b8a61c77508d0e032ea760d0f45a051e
Maykel Arias Torres <dev0006@simelo.tech>
2020-02-29 17:20:48 -0500

    [travis] refs #5 Added install 'mesa-common-dev' in Linux

--- ci-scripts/install-travis-gcc.sh
+++ ci-scripts/install-travis-gcc.sh
@@ -3,6 +3,8 @@
 
 if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
   sudo apt-get install -qq gcc-6 g++-6
+  sudo apt-get -y install build-essential libglu1-mesa-dev libpulse-dev libglib2.0-dev mesa-common-dev
+  sudo apt-get --no-install-recommends install libqt*5-dev qt*5-dev qml-module-qtquick-* qt*5-doc-html
 fi
 
 if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
@@ -12,10 +14,9 @@ if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
   brew list --versions gcc
   echo 'Creating gcc@64 formula'
   cd "$(brew --repository)/Library/Taps/homebrew/homebrew-core"
-  git show 42d31bba7772fb01f9ba442d9ee98b33a6e7a055:Formula/gcc\@6.rb | grep -v 'fails_with' > Formula/gcc\@6.rb
+  git show 42d31bba7772fb01f9ba442d9ee98b33a6e7a055:Formula/gcc\@6.rb | grep -v 'fails_with' >Formula/gcc\@6.rb
   echo 'Installing gcc@6 (6.4.0-2)'
   brew install gcc\@6 || brew link --overwrite gcc\@6
 fi
 
 cd $TRAVIS_BUILD_DIR
-
