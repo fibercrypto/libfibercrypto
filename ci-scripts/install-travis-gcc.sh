0b7669d5ab7a6283b2b53ff2c401690bcf3f435f
Maykel Arias Torres <dev0006@simelo.tech>
2020-03-03 20:59:09 -0500

    [travis] refs #5 Update list apt in `install-travis-gcc.sh`

--- ci-scripts/install-travis-gcc.sh
+++ ci-scripts/install-travis-gcc.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
+  sudo apt-get update
   sudo apt-get install  gcc g++ -y
   sudo apt-get -y install build-essential libgl1-mesa-dev --install-suggests
   go get -u -v github.com/therecipe/qt/cmd/...
