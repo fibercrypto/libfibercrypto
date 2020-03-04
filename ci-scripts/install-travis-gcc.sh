238d501de3012335ae69468a7246a5a5a3671f23
Maykel Arias Torres <dev0006@simelo.tech>
2020-03-04 16:43:48 -0500

    [travis] refs #5 Commit install QT

--- ci-scripts/install-travis-gcc.sh
+++ ci-scripts/install-travis-gcc.sh
@@ -3,8 +3,8 @@
 if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
   sudo apt-get update
   sudo apt-get install -y  gcc g++ libqt5multimedia5 qtmultimedia5-dev libqt5designer5 libqt5quick5 qttools5-dev build-essential libgl1-mesa-dev --install-suggests
-  go get -u -v github.com/therecipe/qt/cmd/...
-  (qtsetup -test=false | true)
+  #go get -u -v github.com/therecipe/qt/cmd/...
+  #(qtsetup -test=false | true)
 fi
 
 if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
@@ -13,8 +13,8 @@ if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
 
   echo 'Install QT'
   brew install qt5
-  go get -u -v github.com/therecipe/qt/cmd/...
-  qtsetup -test=false || true
+  #go get -u -v github.com/therecipe/qt/cmd/...
+  #qtsetup -test=false || true
 fi
 
 cd $TRAVIS_BUILD_DIR
