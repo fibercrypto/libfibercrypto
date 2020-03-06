8a61a8ac73b67dcd63517f55dafc26860ae614df
Maykel Arias Torres <dev0006@simelo.tech>
2020-03-06 08:57:42 -0500

    [travis] refs #5 INstall optinal package the QT

--- ci-scripts/install-travis-gcc.sh
+++ ci-scripts/install-travis-gcc.sh
@@ -2,9 +2,9 @@
 
 if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
   sudo apt-get update
-  sudo apt-get install -y  gcc g++ libqt5multimedia5 qtmultimedia5-dev libqt5designer5 libqt5quick5 qttools5-dev build-essential libgl1-mesa-dev --install-suggests
+  sudo apt-get install -y  gcc g++ build-essential libgl1-mesa-dev libfontconfig1-dev libfreetype6-dev libx11-dev libxext-dev libxfixes-dev libxi-dev libxrender-dev libxcb1-dev libx11-xcb-dev libxcb-glx0-dev libxkbcommon-x11-dev
   go get -u -v github.com/therecipe/qt/cmd/...
-  (qtsetup -test=false | true)
+  (qtsetup -docker -test=false | true)
 fi
 
 if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
@@ -13,7 +13,6 @@ if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
   echo 'Install QT'
   brew install qt5
   go get -u -v github.com/therecipe/qt/cmd/...
-  qtsetup -test=false || true
 fi
 
 cd $TRAVIS_BUILD_DIR
