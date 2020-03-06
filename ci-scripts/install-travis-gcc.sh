1f22fd9ad4261417df77d8dc5b919d6c378b9d98
Maykel Arias Torres <dev0006@simelo.tech>
2020-03-06 09:06:27 -0500

    [travis] refs #5 Using default package de QTCreator

--- ci-scripts/install-travis-gcc.sh
+++ ci-scripts/install-travis-gcc.sh
@@ -4,7 +4,7 @@ if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
   sudo apt-get update
   sudo apt-get install -y  gcc g++ build-essential libgl1-mesa-dev libfontconfig1-dev libfreetype6-dev libx11-dev libxext-dev libxfixes-dev libxi-dev libxrender-dev libxcb1-dev libx11-xcb-dev libxcb-glx0-dev libxkbcommon-x11-dev
   go get -u -v github.com/therecipe/qt/cmd/...
-  (qtsetup -docker -test=false | true)
+  (qtsetup -test=false | true)
 fi
 
 if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
