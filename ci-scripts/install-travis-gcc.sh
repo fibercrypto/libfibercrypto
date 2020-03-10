c25b191f194a36a9500f1f7d94a0e0c5331f41f5
Maykel Arias Torres <dev0006@simelo.tech>
2020-03-10 09:45:10 -0400

    [travis] refs #5 Limit time in OSX and include new package install en GNU

--- ci-scripts/install-travis-gcc.sh
+++ ci-scripts/install-travis-gcc.sh
@@ -2,8 +2,8 @@
 
 if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
   sudo apt-get update
-  sudo apt-get install -y libgl1-mesa-dev gcc g++ build-essential libqt5multimedia5 libqt5multimedia5-plugins libqt5multimediaquick-p5 libqt5multimediawidgets5
-  go get -u -v github.com/therecipe/qt/cmd/...
+  sudo apt-get install -y libgl1-mesa-dev gcc g++ build-essential libqt5multimedia5 libpulse-mainloop-glib0
+  go get -u github.com/therecipe/qt/cmd/...
   (qtsetup -test=false | true)
 fi
 
@@ -11,7 +11,7 @@ if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
   echo 'Updating packages database'
   brew update
   echo 'Install QT'
-  export GO111MODULE=off; go get -v github.com/therecipe/qt/cmd/... && $(go env GOPATH)/bin/qtsetup test && $(go env GOPATH)/bin/qtsetup -test=false
+  export GO111MODULE=off; go get github.com/therecipe/qt/cmd/... && $(go env GOPATH)/bin/qtsetup -test=false
 fi
 
 cd $TRAVIS_BUILD_DIR
