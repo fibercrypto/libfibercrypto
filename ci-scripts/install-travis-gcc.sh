72ccc314f9ca3ad4db427783799c581816ac16b9
Maykel Arias Torres <dev0006@simelo.tech>
2020-03-09 21:28:43 -0400

    [travis] refs #5  Install `Libqt5multimedia.so`

--- ci-scripts/install-travis-gcc.sh
+++ ci-scripts/install-travis-gcc.sh
@@ -2,7 +2,7 @@
 
 if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
   sudo apt-get update
-  sudo apt-get install -y  gcc g++ build-essential libgl1-mesa-dev libfontconfig1-dev libfreetype6-dev libx11-dev libxext-dev libxfixes-dev libxi-dev libxrender-dev libxcb1-dev libx11-xcb-dev libxcb-glx0-dev libxkbcommon-x11-dev
+  sudo apt-get install -y  gcc g++ build-essential libqt5multimedia5 libqt5multimedia5-plugins libqt5multimediaquick-p5 libqt5multimediawidgets5
   go get -u -v github.com/therecipe/qt/cmd/...
   (qtsetup -test=false | true)
 fi
