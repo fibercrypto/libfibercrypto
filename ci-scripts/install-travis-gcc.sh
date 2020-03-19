40b0ec272f042fed9994e704e1d93e6b25ace5b0
Maykel Arias Torres <dev0006@simelo.tech>
2020-03-19 08:56:00 -0400

    [travis] refs #5 Remove old definitions the brew

--- ci-scripts/install-travis-gcc.sh
+++ ci-scripts/install-travis-gcc.sh
@@ -8,9 +8,6 @@ if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
 fi
 
 if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
-  echo 'Updating packages database'
-  brew update
-  echo 'Install QT'
   export GO111MODULE=off; go get github.com/therecipe/qt/cmd/... && $(go env GOPATH)/bin/qtsetup -test=false
 fi
 
